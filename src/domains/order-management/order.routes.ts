e166c114eeff17e260cb398f2542ed1cb8ee410b
daughtly.svats <sergey.vatz@gmail.com>
2025-04-28 23:15:25 +0200

    refactor(order-management): mv the logic to the corresponding files(repository/routes/service/types

--- src/domains/order-management/order.routes.ts
+++ src/domains/order-management/order.routes.ts
@@ -1,74 +1,47 @@
 import { FastifyInstance } from 'fastify'
+
+import { Order } from './order.types'
 import {
-    getOrders,
-    getOrderById,
+    fetchOrders,
+    fetchOrderById,
+    fetchOrderTotal,
     createOrder,
     updateOrder,
     deleteOrder,
-} from '../../mockDatabaseClient'
-import { Order } from '../../types'
+} from './order.service'
 
 export default async function orderRoutes(server: FastifyInstance) {
     server.get('/', async () => {
-        return getOrders()
+        return fetchOrders()
     })
 
-    server.get('/:id', async (request, reply) => {
+    server.get('/:id', async request => {
         const { id } = request.params as { id: string }
-        const order = getOrderById(parseInt(id, 10))
-
-        if (order) {
-            return order
-        }
-
-        reply.status(404).send({ message: 'Order not found' })
+        return fetchOrderById(parseInt(id, 10))
     })
 
-    server.get('/:id/order-total', async (request, reply) => {
+    server.get('/:id/order-total', async request => {
         const { id } = request.params as { id: string }
-        const order = getOrderById(parseInt(id, 10))
-
-        if (order) {
-            const total = order.products.reduce(
-                (acc, product) => acc + product.price * product.count,
-                0,
-            )
-
-            return reply.status(201).send({
-                total,
-            })
-        }
-
-        reply.status(404).send({ message: 'Order not found' })
+        return fetchOrderTotal(parseInt(id, 10))
     })
 
-    server.post('/', async (request, reply) => {
+    server.post('/', async request => {
         const { userId, products } = request.body as Order
-
-        const newOrder = createOrder(userId, products)
-
-        reply.status(201).send(newOrder)
+        return createOrder(userId, products)
     })
 
-    server.put('/:id', async (request, reply) => {
+    server.put('/:id', async request => {
         const { id } = request.params as { id: string }
         const updatedOrder = request.body as Partial<Order>
-        const order = updateOrder(parseInt(id, 10), updatedOrder)
-
-        if (order) {
-            return reply.status(201).send(order)
-        }
-
-        reply.status(404).send({ message: 'Order not found' })
+        return updateOrder(parseInt(id, 10), updatedOrder)
     })
 
     server.delete('/:id', async (request, reply) => {
         const { id } = request.params as { id: string }
         const success = deleteOrder(parseInt(id, 10))
         if (success) {
-            reply.status(204).send()
-        } else {
-            reply.status(404).send({ message: 'Order not found' })
+            return reply.status(204).send()
         }
+        return reply.status(404).send({ message: 'Order not found' })
     })
 }
