9fc13fcbf22280a245d0fa060ab6e93ab516ee2c
daughtly.svats <sergey.vatz@gmail.com>
2025-04-28 22:51:40 +0200

    refactor(order.routes): move order-related routes to the associated domain

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
 import fastify from 'fastify'
-import {
-    getOrders,
-    getOrderById,
-    createOrder,
-    updateOrder,
-    deleteOrder,
-} from './mockDatabaseClient'
-import { Order } from './types'
+
 import swagger from '@fastify/swagger'
 import swaggerUI from '@fastify/swagger-ui'
 import path from 'path'
+import orderRoutes from './domains/order-management/order.routes'
 
 const server = fastify({ logger: true })
 
@@ -43,68 +37,7 @@ server.register(swaggerUI, {
     transformSpecificationClone: true,
 })
 
-server.get('/orders', async () => {
-    return getOrders()
-})
-
-server.get('/orders/:id', async (request, reply) => {
-    const { id } = request.params as { id: string }
-    const order = getOrderById(parseInt(id, 10))
-
-    if (order) {
-        return order
-    }
-
-    reply.status(404).send({ message: 'Order not found' })
-})
-
-server.get('/orders/:id/order-total', async (request, reply) => {
-    const { id } = request.params as { id: string }
-    const order = getOrderById(parseInt(id, 10))
-
-    if (order) {
-        const total = order.products.reduce(
-            (acc, product) => acc + product.price * product.count,
-            0,
-        )
-
-        return reply.status(201).send({
-            total,
-        })
-    }
-
-    reply.status(404).send({ message: 'Order not found' })
-})
-
-server.post('/orders', async (request, reply) => {
-    const { userId, products } = request.body as Order
-
-    const newOrder = createOrder(userId, products)
-
-    reply.status(201).send(newOrder)
-})
-
-server.put('/orders/:id', async (request, reply) => {
-    const { id } = request.params as { id: string }
-    const updatedOrder = request.body as Partial<Order>
-    const order = updateOrder(parseInt(id, 10), updatedOrder)
-
-    if (order) {
-        return reply.status(201).send(order)
-    }
-
-    reply.status(404).send({ message: 'Order not found' })
-})
-
-server.delete('/orders/:id', async (request, reply) => {
-    const { id } = request.params as { id: string }
-    const success = deleteOrder(parseInt(id, 10))
-    if (success) {
-        reply.status(204).send()
-    } else {
-        reply.status(404).send({ message: 'Order not found' })
-    }
-})
+server.register(orderRoutes, { prefix: '/orders' })
 
 const start = async () => {
     try {
