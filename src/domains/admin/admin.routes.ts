32c2659b1cc287e451d848e5b27d200997ba13db
daughtly.svats <sergey.vatz@gmail.com>
2025-04-29 19:27:09 +0200

    feat(admin): add admin route to GET the user balance

--- src/domains/admin/admin.routes.ts
+++ src/domains/admin/admin.routes.ts
@@ -1,12 +1,12 @@
 import { FastifyInstance } from 'fastify'
-import { calculateUserBalance } from './user.service'
+import { calculateUserBalance } from '../user-management/user.service'
 import { authenticate } from '../../middlewares/authenticaton'
-import { userHasAccess } from '../../middlewares/user-access'
+import { userHasAdminAccess } from '../../middlewares/admin-access'
 
-export default async function userRoutes(server: FastifyInstance) {
+export default async function adminRoutes(server: FastifyInstance) {
     server.get(
-        '/:userId/balance',
-        { preHandler: [authenticate, userHasAccess] },
+        '/users/:userId/balance',
+        { preHandler: [authenticate, userHasAdminAccess] },
         async request => {
             const { userId } = request.params as { userId: string }
             const balance = calculateUserBalance(userId)
