99131a52322be371a9ee4d314aa1e609edc765b9
daughtly.svats <sergey.vatz@gmail.com>
2025-04-29 18:51:54 +0200

    feat(user-management): add calculateUserBalance method

--- src/domains/user-management/user.routes.ts
+++ src/domains/user-management/user.routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from 'fastify'
+import { calculateUserBalance } from './user.service'
 
 export default async function userRoutes(server: FastifyInstance) {
     server.get('/:userId/balance', async request => {
         const { userId } = request.params as { userId: string }
-        return { balance: 100 }
+        const balance = calculateUserBalance(userId)
+        return { balance }
     })
 }
---
+++ src/domains/user-management/user.service.ts
@@ -0,0 +1,14 @@
+import { calculateOrdersSumByUserId } from '../order-management/order.service'
+import { findUserById } from './user.repository'
+
+export function calculateUserBalance(userId: string) {
+    const user = findUserById(userId)
+    if (!user) {
+        throw new Error('The user not found')
+    }
+    const balance = user.balance - calculateOrdersSumByUserId(userId)
+    if (balance < 0) {
+        throw new Error('The balance is negative')
+    }
+    return balance
+}
